262def41e460a98a3059a5e0b5dd6efd290a91e1
Vinod Dhirde <atharv.vinod-dhirde@capgemini.com>
2023-02-14 12:47:25 +0530

    update

--- weather_app/app.js
+++ weather_app/app.js
@@ -3,7 +3,6 @@ const request = require('postman-request')
 
 const url='http://api.weatherstack.com/current?access_key=1417c02c8c6a138f7d82973dd11b6b78&query=37.8267,-122.4233'
 
-request({ url:url},(error, response)=>{
-    const data=JSON.parse(response.body)
-    console.log(data)
+request({ url:url, json:true},(error, response)=>{
+    // console.log(data.current)
 })
