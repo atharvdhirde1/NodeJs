39fdd1e19b310b3a3fc52d1d0fd0d23c5d86427f
Vinod Dhirde <atharv.vinod-dhirde@capgemini.com>
2023-02-14 12:10:37 +0530

    api

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,11 +1,9 @@
-console.log('Starting')
+//request is depricated
+const request = require('postman-request')
 
-setTimeout(()=>{
-    console.log('2 second')
-},2000)
+const url='http://api.weatherstack.com/current?access_key=1417c02c8c6a138f7d82973dd11b6b78&query=37.8267,-122.4233'
 
-setTimeout(()=>{
-    console.log('0 second')
-},0)
-
-console.log('Stoping')
+request({ url:url},(error, response)=>{
+    const data=JSON.parse(response.body)
+    console.log(data)
+})
---
+++ weather_app/basics.js
@@ -0,0 +1,11 @@
+console.log('Starting')
+
+setTimeout(()=>{
+    console.log('2 second')
+},2000)
+
+setTimeout(()=>{
+    console.log('0 second')
+},0)
+
+console.log('Stoping')
