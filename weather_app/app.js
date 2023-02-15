2003960ff2c8aed7776d80214e1515c3fa3af2cf
Vinod Dhirde <atharv.vinod-dhirde@capgemini.com>
2023-02-15 10:34:39 +0530

    weather

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,9 +1,58 @@
-//request is depricated
-const request = require('postman-request')
+//using timeout
+// console.log('starting')
+// setTimeout(()=>{
+//     console.log('zero seconds timer')
+// },0)
+// setTimeout(()=>{
+//     console.log('2 seconds timer')
+// },2000)
+// console.log('stopping')
 
-const url='http://api.weatherstack.com/current?access_key=1417c02c8c6a138f7d82973dd11b6b78&query=37.8267,-122.4233'
+// const request = require('postman-request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+//const url = 'http://api.weatherstack.com/current?access_key=a3ee7bf4bfd44e3e8aa2c0ecaec3ad37&query=37.8267,-122.4233&units=f'
+// request({url:url, json:true},(error,response)=>{
+//     console.log(response.body.current)
+//     // const data = JSON.parse(response.body)
+//     // console.log(data.current)
 
-request({ url:url, json:true},(error, response)=>{
-    // console.log(data.current)
-    console.log("It is currently "+ response.body.current.temperature +" degress out. It feels like "+ response.body.current.feelslike+" degree" )
-})
+// })
+
+//challenge: print a small forecast to the user.
+//it prints 'It is currently 58.55 degrees out.There is 0% chances of rain.
+//request({url:url, json:true},(error,response)=>{
+    // console.log('It is currently ' + response.body.current.temperature + ' degrees out.' + 'There is '+ response.body.current.precip + ' % chances of rain.')
+    //console.log(response.body.current.weather_descriptions[0] + '.It is currently ' + response.body.current.temperature + ' degrees out.' + 'It feels like '+ response.body.current.feelslike + ' degrees out.')
+//}) 
+
+//geocoding
+// const geocodeURL ='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicG9vbmFtayIsImEiOiJjbGU0M2tyeHkwOHowM29tcm5iNHZ3d3ZiIn0.gtZ4BY-VFhi3FijNyj-bFg'
+// request({url:geocodeURL,json:true},(error,response)=>{
+//     const latitude = response.body.features[0].center[1]
+//     const longitude = response.body.features[0].center[0]
+//     console.log(latitude,longitude)
+
+// })
+
+
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
