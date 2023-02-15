0c17ebe44ab1ce72c4b5743aae20acd3bfd4c3b1
Vinod Dhirde <atharv.vinod-dhirde@capgemini.com>
2023-02-15 19:46:38 +0530

    weather done

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,67 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
-// console.log(__dirname) // tells the folder name
-// console.log(__filename) // tells the file name
-const app= express()
-const publicDirectoryPath= path.join(__dirname, '../public')
+const app = express()
 
-app.set('view engine','hbs')
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('',(req,res)=>{
-    res.render('index')
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Atharv Dhirde'
+    })
 })
 
-app.get('/weather',(req,res)=>{
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Atharv Dhirde'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Atharv Dhirde'
+    })
+})
+
+app.get('/weather', (req, res) => {
     res.send({
-        'forecast':'It is sunny',
-        'location':'India'
-        })
+        forecast: 'It is snowing',
+        location: 'India'
+    })
 })
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000')
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Atharv Dhirde',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Atharv Dhirde',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
